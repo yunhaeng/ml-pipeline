4c52b849bb3fb5a5ccdd6bbff8817ab368c2763f
yunhaeng <dkfk591@gmail.com>
2024-12-10 15:31:08 +0900

    [CRT] airflow_install.sh

---
+++ shell/airflow_install.sh
@@ -0,0 +1,11 @@
+export AIRFLOW_HOME=~/airflow
+
+AIRFLOW_VERSION=2.9.2
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+
+#The standalone command will initialise the database, make a user.
+airflow standalone
