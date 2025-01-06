6fc13817db5f850da70a82f5e484eb33b65ab3e8
yunhaeng <dkfk591@gmail.com>
2025-01-06 21:27:47 +0900

    [CRT] Add insert predict to mariaDB

--- models/ineligible_loan_model/model/prediction.py
+++ models/ineligible_loan_model/model/prediction.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import joblib
+from sqlalchemy import create_engine
 
 feature_store_url = os.getenv("FEATURE_STORE_URL", "")
 model_output_home = os.getenv("MODEL_OUTPUT_HOME", "")
@@ -35,15 +36,64 @@ class prediction:
             "self_employed_Yes",
         ]
         
-        x_pred = self.load_data(features)
+
+        loan_df = self.load_data()
+        x_pred = loan_df[features]
 
         model_file_name = f"{self._model_name}.joblib"
         model = joblib.load(f"{model_output_home}/model_output/{model_file_name}")
 
         print(f"logistic_model = {model}")
-        y_pred = model.predict(x_pred)
         
+        #predict
+        y_pred = model.predict(x_pred)
+        print("Target on test data (sample 20) =", y_pred[:20])
 
-    def load_data(self, features):
+        #predict_proba
+        y_pred_proba = model.predict_proba(x_pred)
+        print(f"test_predict_proba (sample 5) = {y_pred_proba[:5]}")
+
+        #예측 라벨이 1인 경우의 probability 추출
+        test_proba = pd.DataFrame(y_pred_proba)[1].to_list()
+        print(f"test_proba (sample 5) = {test_proba[:5]}")
+
+        #예측 결과를 DataFrame으로 변환
+        data = {
+            "base_dt": self._base_day,
+            "applicant_id": loan_df["applicant_id"].to_list(),
+            "predict": y_pred,
+            "probability": test_proba
+        }
+        test_predicted = pd.DataFrame(data)
+        print(f"test_predicted = {test_predicted}")
+
+        #예측 결과 db 저장
+        print(f"feature_store_url = {feature_store_url}")
+        print(test_predicted.dtypes)
+        engine = create_engine(feature_store_url)
+
+        print(data)
+        data = test_predicted.to_dict(orient="records")
+        with engine.connect() as conn:
+            init_sql = f"""
+                delete
+                    from mlops.ineligible_loan_model_result
+                     where base_dt = '{self._base_day}'
+            """
+            conn.execute(init_sql)
+
+            for i in range(len(data)):
+                insert_sql = """
+                    insert into mlops.ineligible_loan_model_result
+                    ({columns})
+                    VALUES ({values})
+                """.format(
+                    columns=", ".join(data[i].keys()),
+                    values=", ".join([f"'{value}'" for value in data[i].values()])
+                )
+
+                conn.execute(insert_sql)
+
+    def load_data(self):
         loan_df = pd.read_csv(f"{self._data_preparation_path}/{self._feature_file_name}")
-        return loan_df[features].values
+        return loan_df
