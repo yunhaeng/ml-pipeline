47e741d2cff45c21f936ab6c6185ebed397f8987
yunhaeng <dkfk591@gmail.com>
2025-01-07 21:07:16 +0900

    [CRT] Refactoring prediction.py

--- models/ineligible_loan_model/model/prediction.py
+++ models/ineligible_loan_model/model/prediction.py
@@ -40,32 +40,10 @@ class prediction:
         loan_df = self.load_data()
         x_pred = loan_df[features]
 
-        model_file_name = f"{self._model_name}.joblib"
-        model = joblib.load(f"{model_output_home}/model_output/{model_file_name}")
-
-        print(f"logistic_model = {model}")
+        model = self._load_model()
         
-        #predict
-        y_pred = model.predict(x_pred)
-        print("Target on test data (sample 20) =", y_pred[:20])
-
-        #predict_proba
-        y_pred_proba = model.predict_proba(x_pred)
-        print(f"test_predict_proba (sample 5) = {y_pred_proba[:5]}")
-
-        #예측 라벨이 1인 경우의 probability 추출
-        test_proba = pd.DataFrame(y_pred_proba)[1].to_list()
-        print(f"test_proba (sample 5) = {test_proba[:5]}")
-
-        #예측 결과를 DataFrame으로 변환
-        data = {
-            "base_dt": self._base_day,
-            "applicant_id": loan_df["applicant_id"].to_list(),
-            "predict": y_pred,
-            "probability": test_proba
-        }
-        test_predicted = pd.DataFrame(data)
-        print(f"test_predicted = {test_predicted}")
+        # Make predictions using the loaded model
+        data, test_predicted = self._predict(loan_df, x_pred, model)
 
         #예측 결과 db 저장
         print(f"feature_store_url = {feature_store_url}")
@@ -93,6 +71,36 @@ class prediction:
                 )
 
                 conn.execute(insert_sql)
+
+    def _predict(self, loan_df, x_pred, model):
+        y_pred = model.predict(x_pred)
+        print("Target on test data (sample 20) =", y_pred[:20])
+
+        # Predict the probability of each class for the input features
+        y_pred_proba = model.predict_proba(x_pred)
+        print(f"test_predict_proba (sample 5) = {y_pred_proba[:5]}")
+
+        # Extract probability for predicted label 1
+        test_proba = pd.DataFrame(y_pred_proba)[1].to_list()
+        print(f"test_proba (sample 5) = {test_proba[:5]}")
+
+        # Convert prediction results to DataFrame
+        data = {
+            "base_dt": self._base_day,
+            "applicant_id": loan_df["applicant_id"].to_list(),
+            "predict": y_pred,
+            "probability": test_proba
+        }
+        test_predicted = pd.DataFrame(data)
+        print(f"test_predicted = {test_predicted}")
+        return data,test_predicted
+
+    def _load_model(self):
+        model_file_name = f"{self._model_name}.joblib"
+        model = joblib.load(f"{model_output_home}/model_output/{model_file_name}")
+
+        print(f"logistic_model = {model}")
+        return model
 
     def load_data(self):
         loan_df = pd.read_csv(f"{self._data_preparation_path}/{self._feature_file_name}")
